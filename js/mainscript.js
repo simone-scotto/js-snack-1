047bc0dd46bea17ed8a526630176f42814f1ff0e
simone-scotto <simone.scottotkd@gmail.com>
2025-05-30 10:51:52 +0200

    numero maggiore

---
+++ js/mainscript.js
@@ -0,0 +1,17 @@
+let firstNumber = Number(prompt("scegli un numero"));
+
+
+
+
+let secondNumber = Number(prompt("scegli un altro numero"));
+
+
+
+
+if (firstNumber > secondNumber) {
+    console.log(firstNumber);
+    
+} else {
+    console.log(secondNumber);
+    
+}
